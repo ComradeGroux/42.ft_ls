f15e4363e98463190008e378fa7f1334d0473ac2
Victor Groux <victorgroux99@gmail.com>
2024-02-01 10:22:48 +0100

    flag in bitwise

--- headers/ft_ls.h
+++ headers/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 08:26:20 by vgroux            #+#    #+#             */
-/*   Updated: 2024/02/01 09:08:30 by vgroux           ###   ########.fr       */
+/*   Updated: 2024/02/01 09:57:17 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define flag_a = 0b00001
-#define flag_l = 0b00010
-#define flag_R = 0b00100
-#define flag_r = 0b01000
-#define flag_t = 0b10000
+#define MAN_LS "./ft_ls [OPTION]... [FILE]...\n\
+[OPTION] are: \n\
+\t-a\tdo not ignore entries starting with .\n\
+\t-l\tuse a long listing format\n\
+\t-r\treverse order while sorting\n\
+\t-R\tlist subdirectories recursively\n\
+\t-t\tsort by time, newest first"
 
+#define FLAG_a 				0b000001
+#define FLAG_l				0b000010
+#define FLAG_R				0b000100
+#define FLAG_r				0b001000
+#define FLAG_t				0b010000
+#define FLAG_MULTI			0b100000
 
+int		init(int argc, char** argv);
+void	ls(int flag);
+void	ls_recur(int flag);
 void	ft_error(char* str);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vgroux <vgroux@student.42lausanne.ch>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 09:05:28 by vgroux            #+#    #+#             */
-/*   Updated: 2024/02/01 09:16:08 by vgroux           ###   ########.fr       */
+/*   Updated: 2024/02/01 10:21:28 by vgroux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,102 @@
 
 int	main(int argc, char** argv)
 {
-	(void)argv;
-	if (argc >= 2)
+	int	flag = init(argc, argv);
+	printf("flag is %d\n", flag);
+	if (flag != -1)
 	{
-
+		if ((flag & FLAG_R) != 0) // if '-R' is active, ls must be subdirectories recursive
+			ls_recur(flag);
+		else
+			ls(flag);
 	}
 	else
-		ft_error("./ft_ls [OPTION]... [FILE]...\n[OPTION] are: \n-a\tdo not ignore entries starting with .\n-l\tuse a long listing format\n-r\treverse order while sorting\n-R\tlist subdirectories recursively\n-t\tsort by time, newest first");
+		ft_error("An error occured during the init");
 	return (0);
+}
+
+void	ls(int flag)
+{
+	(void)flag;
+	ft_printf("ls classique\n");
+}
+
+void	ls_recur(int flag)
+{
+	(void)flag;
+	ft_printf("ls recursif\n");
+}
+
+/**
+ * 	ls "-a" "-l"			OK
+ * 	ls -al					OK
+ * 	ls -aaaaaaaaaaa -l		OK
+ * 	ls " -a"				failed
+ * 	ls "-a -l"				failed
+ * 	ls "-a" -l "headers/"	OK
+ * 	ls "-a" -l "headers/ "	failed
+ * 	ls headers/ -a			OK
+ * 	ls headers/ -a srcs/ -l	OK
+ */ 
+int	init(int argc, char** argv)
+{
+	int	flag = 0;
+
+	if (argc == 1)
+		return (0);
+	else
+	{
+		int	j;
+		int	i = 1;
+
+		while (argv[i])
+		{
+			if (argv[i][0] != '-')
+			{
+				i++;
+				continue;
+			}
+			else
+			{
+				j = 1;
+				while (argv[i][j])
+				{
+					char	c = argv[i][j];
+					
+					switch (c)
+					{
+						case 'a':
+							flag = flag | FLAG_a;
+							break;
+						case 'l':
+							flag = flag | FLAG_l;
+							break;
+						case 'R':
+							flag = flag | FLAG_R;
+							break;
+						case 'r':
+							flag = flag | FLAG_r;
+							break;
+						case 't':
+							flag = flag | FLAG_t;
+							break;
+						default:
+							return (-1);
+							break;
+					}
+					j++;
+				}
+			}
+			i++;
+		}
+	}
+	return (flag);
 }
 
 void	ft_error(char* str)
 {
 	ft_putstr_fd("Error: ", 2);
 	ft_putendl_fd(str, 2);
+	ft_putstr_fd("Usage: ", 2);
+	ft_putendl_fd(MAN_LS, 2);
 }
